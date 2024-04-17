28baffb7e806e46b260b91c00ad8f991611e7500
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-04-17 17:43:03 -0400

    Add skeletons for split-merge algorithm

---
+++ src/algorithms/calorimetry/TrackClusterMergeSplitter.cc
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2024 Derek Anderson
+
+#define EICRECON_TRACKCLUSTERMERGESPLITTER_CC
+
+#include "TrackClusterMergeSplitter.h"
+
+namespace eicrecon {
+
+  // --------------------------------------------------------------------------
+  //! Initialize algorithm
+  // --------------------------------------------------------------------------
+  void TrackClusterMergeSplitterAlgorithm::init() {
+
+    /* TODO fill in */
+
+  }  // end 'init()'
+
+  // --------------------------------------------------------------------------
+  //! Process inputs
+  // --------------------------------------------------------------------------
+  void TrackClusterMergeSplitter::process(
+    const TrackClusterMergeSplitter::Input& input,
+    const TrackClusterMergeSplitter::Output& output
+  ) const {
+
+    /* TODO fill in */
+
+  }  // end 'process(Input&, Output&)'
+
+}  // end eicrecon namespace
