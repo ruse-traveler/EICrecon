b09bba4ab759e199c485689dc3a0ecb63dc4989d
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-01-15 13:55:17 -0500

    Add particle subdirectories to cmake files

--- update.sh
+++ update.sh
@@ -6,3 +6,6 @@ rm -rv src/global/particle/
 cp -rv ../development/ForParticleFlow/mark2/new/algorithms/particle/ ./src/algorithms/
 cp -rv ../development/ForParticleFlow/mark2/new/factories/particle/ ./src/factories/
 cp -rv ../development/ForParticleFlow/mark2/new/global/particle/ ./src/global/
+cp -v ../development/ForParticleFlow/mark2/new/algorithms/CMakeLists.txt ./src/algorithms/
+cp -v ../development/ForParticleFlow/mark2/new/factories/CMakeLists.txt ./src/factories/
+cp -v ../development/ForParticleFlow/mark2/new/global/CMakeLists.txt ./src/global/
