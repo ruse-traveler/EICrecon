7b780c5cc70c97bf7ce2e6f89655cf4d2a991c6f
ruse-traveler <derek.murphy.anderson@protonmail.com>
2026-01-05 11:49:32 -0500

    Fix typos in factory

--- src/factories/particle/TrackProtoClusterMatchPromoter_factory.h
+++ src/factories/particle/TrackProtoClusterMatchPromoter_factory.h
@@ -3,8 +3,8 @@
 
 #pragma once
 
-#include "extensions/jana/JOmniFactory.h"
 #include "algorithms/particle/TrackProtoClusterMatchPromoter.h"
+#include "extensions/jana/JOmniFactory.h"
 
 namespace eicrecon {
 
@@ -16,32 +16,28 @@ public:
 
 private:
   // pointer to algorithm
-  std::unique_pt<AlgoT> m_algo;
+  std::unique_ptr<AlgoT> m_algo;
 
   // input collections
   PodioInput<edm4eic::TrackProtoClusterMatch> m_track_proto_match_input{this};
-  PodioInput<edm4eic::ProtoClusterCollection> m_proto_input{this};
-  PodioInput<edm4eic::ClusterCollection> m_clust_input{this};
+  PodioInput<edm4eic::ProtoCluster> m_proto_input{this};
+  PodioInput<edm4eic::Cluster> m_clust_input{this};
 
   // output collection
   PodioOutput<edm4eic::TrackClusterMatch> m_track_clust_match_output{this};
-
-  // services
-  Service<AlgorithmsInit_service> m_algoInitSvc{this};
 
 public:
   ///! Configures algorithm
   void Configure() {
     m_algo = std::make_unique<AlgoT>(GetPrefix());
-    m_algo->level(static_cast<algorithm::LogLevel>(logger()->level()));
-    m_algo->applyConfig(config());
+    m_algo->level(static_cast<algorithms::LogLevel>(logger()->level()));
     m_algo->init();
   }
 
   ///! Primary algorithm call
   void Process(int32_t /*run_number*/, uint64_t /*event_number*/) {
     m_algo->process({m_track_proto_match_input(), m_proto_input(), m_clust_input()},
-                    {m_track_clust_match_output.get()});
+                    {m_track_clust_match_output().get()});
   }
 }; // end TrackProtoClusterMatchPromoter_factory
 
